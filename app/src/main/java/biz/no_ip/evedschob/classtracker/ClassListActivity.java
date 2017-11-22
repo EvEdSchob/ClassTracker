5b944a257e84539aa3fe2aff0ce97c79fd420d5d
Evan Schober <evedschob@live.com>
2017-11-22 11:48:26 -0500

    Initial Commit

--- app/src/main/java/biz/no_ip/evedschob/classtracker/ClassListActivity.java
+++ app/src/main/java/biz/no_ip/evedschob/classtracker/ClassListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class ClassListActivity extends FragmentActivity {
 
+    //Initial commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
